d4b4ebd544f67444b6bade3685e5cba8c1da429b
Alberta <alberta1.amani@yahoo.co.uk>
2025-04-02 22:47:14 +0100

    fix: server.js

--- Amazona/backend/server.js
+++ Amazona/backend/server.js
@@ -4,6 +4,12 @@ import data from "./data.js";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoute from "./routes/auth.js";
+import productRoute from "./routes/product.js";
+import stripeRoute from "./routes/stripe.js";
+import orderRoute from "./routes/order.js";
+import userRoute from "./routes/user.js";
+import cartRoute from "./routes/cart.js";
 
 dotenv.config();
 const app = express();
@@ -34,6 +40,12 @@ app.get("/api/products/:id", (req, res) => {
     res.status(404).send({ message: "Product Not Found!" });
   }
 });
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/stripe", stripeRoute);
+app.use("/api/product", productRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(5001, () => {
   console.log("serve at http://localhost:5001");
